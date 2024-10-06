be80912036f788e14955cda191553f6d8e665f89
jdalton <john.david.dalton@gmail.com>
2024-10-05 23:15:43 -0400

    Add es6-object-assign tests

--- scripts/constants.js
+++ scripts/constants.js
@@ -423,10 +423,11 @@ const skipTestsByEcosystem = Object.freeze({
   __proto__: null,
   npm: new Set([
     // Has no unit tests.
-    // https://github.com/hyrious/bun.lockb/tree/v0.0.3
+    // https://github.com/hyrious/bun.lockb/tree/v0.0.4
     '@hyrious/bun.lockb',
     'hyrious__bun.lockb',
-    // Has known test fails for some Node versions and platforms:
+    // Has known test fails for some Node versions and platforms, but passes in
+    // CI environments for the time being.
     // https://github.com/es-shims/Date/issues/3
     // https://github.com/es-shims/Date/tree/v2.0.5
     ...(ENV.CI ? [] : ['date']),
@@ -434,17 +435,18 @@ const skipTestsByEcosystem = Object.freeze({
     // https://github.com/rubennorte/es6-object-assign/tree/v1.1.0
     'es6-object-assign',
     // Has known failures in its package and requires running tests in browser.
-    // https://github.com/tvcutsem/harmony-reflect/tree/v1.6.2
+    // https://github.com/tvcutsem/harmony-reflect/tree/v1.6.2/test
     'harmony-reflect',
-    // The package tests don't account for the `require('node:util/types).isRegExp`
-    // method having no observable side-effects and assumes the "getOwnPropertyDescriptor"
+    // Tests don't account for `is-regex` backed by `require('node:util/types).isRegExp`
+    // which triggers no proxy traps and assumes instead that the "getOwnPropertyDescriptor"
     // trap will be triggered by `Object.getOwnPropertyDescriptor(value, 'lastIndex')`.
     // https://github.com/inspect-js/is-regex/issues/35
-    // https://github.com/inspect-js/is-regex/tree/v1.1.4
+    // https://github.com/inspect-js/is-regex/blob/v1.1.4/test/index.js
     'is-regex',
-    // Has known failures in its package.
+    // Tests assume Buffer.alloc, Buffer.allocUnsafe, and Buffer.allocUnsafeSlow
+    // throw for a size of 2 * (1 << 30), i.e. 2147483648, which is no longer the case.
     // https://github.com/ChALkeR/safer-buffer/issues/16
-    // https://github.com/ChALkeR/safer-buffer/tree/v2.1.2
+    // https://github.com/ChALkeR/safer-buffer/blob/v2.1.2/tests.js
     'safer-buffer'
   ])
 })
