108d6c7b3d7308edd74395462d74fead9de28325
jdalton <john.david.dalton@gmail.com>
2024-10-28 15:23:36 -0400

    Update d.ts for registry

--- registry/index.d.ts
+++ registry/index.d.ts
@@ -1,42 +1,44 @@
-declare enum Categories {
-  CLEANUP = 'cleanup',
-  LEVELUP = 'levelup',
-  SPEEDUP = 'speedup',
-  TUNEUP = 'tuneup'
-}
-declare type CategoryString = `${Categories}`
 declare enum PURL_Type {
   NPM = 'npm'
 }
-declare type EcosystemString = `${PURL_Type}`
-declare enum Interop {
-  BROWSERIFY = 'browserify',
-  CJS = 'cjs',
-  ESM = 'esm'
-}
-declare type InteropString = `${Interop}`
-declare type Manifest = {
-  [Ecosystem in PURL_Type]: ManifestEntry[]
-}
-declare type ManifestEntry = [string, ManifestEntryData]
-declare type ManifestEntryData = {
-  categories: CategoryString[]
-  interop: InteropString[]
-  license: string
-  name: string
-  package: string
-  version: string
-  deprecated?: boolean
-  engines: { node: string; npm?: string }
-  skipTests?: boolean
-}
-declare interface SocketSecurityRegistry {
-  getManifestData(): Manifest | undefined
-  getManifestData(eco: EcosystemString): ManifestEntry[] | undefined
-  getManifestData(
+declare namespace SocketSecurityRegistry {
+  export enum Categories {
+    CLEANUP = 'cleanup',
+    LEVELUP = 'levelup',
+    SPEEDUP = 'speedup',
+    TUNEUP = 'tuneup'
+  }
+  export type CategoryString = `${Categories}`
+  export type EcosystemString = `${PURL_Type}`
+  export enum Interop {
+    BROWSERIFY = 'browserify',
+    CJS = 'cjs',
+    ESM = 'esm'
+  }
+  export type InteropString = `${Interop}`
+  export type Manifest = {
+    [Ecosystem in PURL_Type]: ManifestEntry[]
+  }
+  export type ManifestEntry = [string, ManifestEntryData]
+  export type ManifestEntryData = {
+    categories: CategoryString[]
+    interop: InteropString[]
+    license: string
+    name: string
+    package: string
+    version: string
+    deprecated?: boolean
+    engines: {
+      node: string
+      npm?: string
+    }
+    skipTests?: boolean
+  }
+  export function getManifestData(): Manifest | undefined
+  export function getManifestData(eco: EcosystemString): ManifestEntry[] | undefined
+  export function getManifestData(
     eco: EcosystemString,
     regPkgName: string
   ): ManifestEntryData | undefined
 }
-declare const registry: SocketSecurityRegistry
-export = registry
+export = SocketSecurityRegistry
