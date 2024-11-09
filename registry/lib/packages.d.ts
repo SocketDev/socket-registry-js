bb132446299c01541d2db481369006db3b3c06f8
jdalton <john.david.dalton@gmail.com>
2024-11-09 11:39:35 -0500

    Update packages.d.ts

--- registry/lib/packages.d.ts
+++ registry/lib/packages.d.ts
@@ -6,139 +6,115 @@ import {
 } from 'pacote'
 import { CategoryString } from '../index'
 
-declare type Exports = Exclude<PackageJson['exports'], undefined>
-declare type PackageJson = NPMCliPackageJson.Content & {
-  socket?: { categories: CategoryString }
-}
-declare class EditablePackageJson extends NPMCliPackageJson {
-  content: Readonly<PackageJson>
-}
-declare type NormalizedPackageJson = Omit<PackageJson, 'repository'> & {
-  repository?: Exclude<PackageJson['repository'], string>
-}
-declare type PacoteOptions = PacoteOptionsRaw & {
-  signal?: AbortSignal
-}
-declare type ExtractOptions = PacoteOptions & {
-  tmpPrefix?: string
-}
-declare interface LicenseNode {
-  license: string
-  exception?: string
-  inFile?: string
-  plus?: boolean
-}
-declare function collectIncompatibleLicenses(
-  licenseNodes: LicenseNode[]
-): LicenseNode[]
-declare function collectLicenseWarnings(licenseNodes: LicenseNode[]): string[]
-declare function createPackageJson(
-  regPkgName: string,
-  directory: string,
-  options: PackageJson
-): PackageJson
-declare function extractPackage(
-  pkgNameOrId: string,
-  options: ExtractOptions,
-  callback: (tmpDirPath: string) => Promise<any>
-): Promise<void>
-declare function fetchPackageManifest(
-  pkgNameOrId: string,
-  options?: PacoteOptions
-): ReturnType<typeof PacoteManifestFn>
-declare function findTypesForSubpath(
-  entryExports: Exports,
-  subpath: string
-): string | undefined
-declare function getSubpaths(entryExports: Exports): string[]
-declare function isConditionalExports(entryExports: Exports): boolean
-declare function isSubpathExports(entryExports: Exports): boolean
-declare function isValidPackageName(value: any): boolean
-declare function normalizePackageJson(
-  pkgJson: PackageJson,
-  options?: { preserve?: string[] }
-): NormalizedPackageJson
-declare function packPackage(
-  spec: string,
-  options?: PacoteOptions & {
-    args?: string[]
-    binPaths?: string[]
-    cmd?: string
-    dryRun?: boolean
-    env?: { [key: string]: string }
-    foregroundScripts?: boolean
-    ignoreScripts?: boolean
-    packDestination?: string
-    scriptShell?: string
-    stdioString?: boolean
+declare namespace PackagesModule {
+  export type Exports = Exclude<PackageJson['exports'], undefined>
+  export type PackageJson = NPMCliPackageJson.Content & {
+    socket?: { categories: CategoryString }
   }
-): Awaited<ReturnType<typeof PacoteTarballFn>>
-declare function readPackageJson(
-  filepath: string,
-  options: { editable: true; preserve?: string[] }
-): Promise<EditablePackageJson>
-declare function readPackageJson(
-  filepath: string,
-  options?: { editable?: false; preserve?: string[] }
-): Promise<PackageJson>
-declare function readPackageJsonSync(
-  filepath: string,
-  options: { editable: true; preserve?: string[] }
-): EditablePackageJson
-declare function readPackageJsonSync(
-  filepath: string,
-  options?: { editable?: false; preserve?: string[] }
-): PackageJson
-declare function resolveEscapedScope(regPkgName: string): string
-declare function resolveGitHubTgzUrl(
-  pkgNameOrId: string,
-  where: string
-): Promise<string>
-declare function resolveOriginalPackageName(regPkgName: string): string
-declare function resolvePackageJsonDirname(filepath: string): string
-declare function resolvePackageJsonEntryExports(
-  entryExports: any
-): Exports | undefined
-declare function resolvePackageJsonPath(filepath: string): string
-declare function resolvePackageLicenses(
-  licenseFieldValue: string,
-  where: string
-): LicenseNode[]
-declare function resolveRegistryPackageName(pkgName: string): string
-declare function toEditablePackageJson(
-  pkgJson: PackageJson,
-  options: { path?: string; preserve?: string[] }
-): Promise<EditablePackageJson>
-declare function toEditablePackageJsonSync(
-  pkgJson: PackageJson,
-  options: { path?: string; preserve?: string[] }
-): EditablePackageJson
-declare function unescapeScope(escapedScope: string): string
-declare const packagesModule: {
-  collectIncompatibleLicenses: typeof collectIncompatibleLicenses
-  collectLicenseWarnings: typeof collectLicenseWarnings
-  createPackageJson: typeof createPackageJson
-  extractPackage: typeof extractPackage
-  fetchPackageManifest: typeof fetchPackageManifest
-  findTypesForSubpath: typeof findTypesForSubpath
-  getSubpaths: typeof getSubpaths
-  isConditionalExports: typeof isConditionalExports
-  isSubpathExports: typeof isSubpathExports
-  isValidPackageName: typeof isValidPackageName
-  normalizePackageJson: typeof normalizePackageJson
-  packPackage: typeof packPackage
-  readPackageJson: typeof readPackageJson
-  readPackageJsonSync: typeof readPackageJsonSync
-  resolveEscapedScope: typeof resolveEscapedScope
-  resolveGitHubTgzUrl: typeof resolveGitHubTgzUrl
-  resolveOriginalPackageName: typeof resolveOriginalPackageName
-  resolvePackageJsonDirname: typeof resolvePackageJsonDirname
-  resolvePackageJsonEntryExports: typeof resolvePackageJsonEntryExports
-  resolvePackageJsonPath: typeof resolvePackageJsonPath
-  resolvePackageLicenses: typeof resolvePackageLicenses
-  resolveRegistryPackageName: typeof resolveRegistryPackageName
-  toEditablePackageJson: typeof toEditablePackageJson
-  toEditablePackageJsonSync: typeof toEditablePackageJsonSync
-  unescapeScope: typeof unescapeScope
+  export class EditablePackageJson extends NPMCliPackageJson {
+    content: Readonly<PackageJson>
+  }
+  export type NormalizedPackageJson = Omit<PackageJson, 'repository'> & {
+    repository?: Exclude<PackageJson['repository'], string>
+  }
+  export type PacoteOptions = PacoteOptionsRaw & {
+    signal?: AbortSignal
+  }
+  export type ExtractOptions = PacoteOptions & {
+    tmpPrefix?: string
+  }
+  export interface LicenseNode {
+    license: string
+    exception?: string
+    inFile?: string
+    plus?: boolean
+  }
+  export function collectIncompatibleLicenses(
+    licenseNodes: LicenseNode[]
+  ): LicenseNode[]
+  export function collectLicenseWarnings(licenseNodes: LicenseNode[]): string[]
+  export function createPackageJson(
+    regPkgName: string,
+    directory: string,
+    options: PackageJson
+  ): PackageJson
+  export function extractPackage(
+    pkgNameOrId: string,
+    options: ExtractOptions,
+    callback: (tmpDirPath: string) => Promise<any>
+  ): Promise<void>
+  export function fetchPackageManifest(
+    pkgNameOrId: string,
+    options?: PacoteOptions
+  ): ReturnType<typeof PacoteManifestFn>
+  export function findTypesForSubpath(
+    entryExports: Exports,
+    subpath: string
+  ): string | undefined
+  export function getSubpaths(entryExports: Exports): string[]
+  export function isConditionalExports(entryExports: Exports): boolean
+  export function isSubpathExports(entryExports: Exports): boolean
+  export function isValidPackageName(value: any): boolean
+  export function normalizePackageJson(
+    pkgJson: PackageJson,
+    options?: { preserve?: string[] }
+  ): NormalizedPackageJson
+  export function packPackage(
+    spec: string,
+    options?: PacoteOptions & {
+      args?: string[]
+      binPaths?: string[]
+      cmd?: string
+      dryRun?: boolean
+      env?: { [key: string]: string }
+      foregroundScripts?: boolean
+      ignoreScripts?: boolean
+      packDestination?: string
+      scriptShell?: string
+      stdioString?: boolean
+    }
+  ): Awaited<ReturnType<typeof PacoteTarballFn>>
+  export function readPackageJson(
+    filepath: string,
+    options: { editable: true; preserve?: string[] }
+  ): Promise<EditablePackageJson>
+  export function readPackageJson(
+    filepath: string,
+    options?: { editable?: false; preserve?: string[] }
+  ): Promise<PackageJson>
+  export function readPackageJsonSync(
+    filepath: string,
+    options: { editable: true; preserve?: string[] }
+  ): EditablePackageJson
+  export function readPackageJsonSync(
+    filepath: string,
+    options?: { editable?: false; preserve?: string[] }
+  ): PackageJson
+  export function resolveEscapedScope(regPkgName: string): string
+  export function resolveGitHubTgzUrl(
+    pkgNameOrId: string,
+    where: string
+  ): Promise<string>
+  export function resolveOriginalPackageName(regPkgName: string): string
+  export function resolvePackageJsonDirname(filepath: string): string
+  export function resolvePackageJsonEntryExports(
+    entryExports: any
+  ): Exports | undefined
+  export function resolvePackageJsonPath(filepath: string): string
+  export function resolvePackageLicenses(
+    licenseFieldValue: string,
+    where: string
+  ): LicenseNode[]
+  export function resolveRegistryPackageName(pkgName: string): string
+  export function toEditablePackageJson(
+    pkgJson: PackageJson,
+    options: { path?: string; preserve?: string[] }
+  ): Promise<EditablePackageJson>
+  export function toEditablePackageJsonSync(
+    pkgJson: PackageJson,
+    options: { path?: string; preserve?: string[] }
+  ): EditablePackageJson
+  export function unescapeScope(escapedScope: string): string
 }
+declare const packagesModule: typeof PackagesModule
 export = packagesModule
