0c000fe3d66f27515e111f046709d7079aa41382
jdalton <john.david.dalton@gmail.com>
2024-12-12 10:57:20 -0500

    Add password to prompts

--- registry/lib/prompts.d.ts
+++ registry/lib/prompts.d.ts
@@ -1,11 +1,13 @@
 import confirm from '@inquirer/confirm'
 import input from '@inquirer/input'
+import password from '@inquirer/password'
 import search from '@inquirer/search'
 import select from '@inquirer/select'
 
 declare const promptsModule: {
   confirm: typeof confirm
   input: typeof input
+  password: typeof password
   search: typeof search
   select: typeof select
 }
--- registry/lib/prompts.js
+++ registry/lib/prompts.js
@@ -4,12 +4,14 @@ const { silentWrapAsync } = require('./functions')
 
 const confirm = silentWrapAsync(require('@inquirer/confirm').default)
 const input = silentWrapAsync(require('@inquirer/input').default)
+const password = silentWrapAsync(require('@inquirer/password').default)
 const search = silentWrapAsync(require('@inquirer/search').default)
 const select = silentWrapAsync(require('@inquirer/select').default)
 
 module.exports = {
   confirm,
   input,
+  password,
   search,
   select
 }
