ad92f3847cb9c9d9b810cc61553415043cfecb0e
jdalton <john.david.dalton@gmail.com>
2024-12-13 09:50:47 -0500

    Set CI spinner interval to real max delay 2147483647

--- scripts/lib/spinner.js
+++ scripts/lib/spinner.js
@@ -9,7 +9,11 @@ const {
 
 const ciSpinner = {
   frames: [''],
-  interval: Infinity
+  // The delay argument is converted to a signed 32-bit integer. This effectively
+  // limits delay to 2147483647 ms, roughly 24.8 days, since it's specified as a
+  // signed integer in the IDL.
+  // https://developer.mozilla.org/en-US/docs/Web/API/Window/setInterval?utm_source=chatgpt.com#return_value
+  interval: 2147483647
 }
 
 function Spinner(options) {
